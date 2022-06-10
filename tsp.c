05b68db6882f0af4b8fb08fe37816aa4cec18467
Vincent Gonnet <vincent.gonnet08@gmail.com>
2022-06-10 21:55:37 +0200

    fixed nearest neighbor algorithm

--- tsp.c
+++ tsp.c
@@ -23,53 +23,53 @@ int result[50];         ///< Result array
 int resultIteration;    ///< Index of the node being added to the result array
 
 /** \brief Function to calculate the closest node to the current node
- * \param currentNode The current node
+ * \param indexNode The current node
  */
 int least(int indexNode)
 {
-    int nc = 999, min = 999;
-    int kmin;
+    int nextNode = 999;   // by default, there is no next node
+    int lowestCost = 999; // by default, there is no minimum, the value is very high so that it will be replaced by the first iteration
 
     for (int i = 0; i < nbNodes; i++)
     {
-        if ((costMatrix[indexNode][i] != 0) && (completed[i] == 0))
-            if (costMatrix[indexNode][i] + costMatrix[i][indexNode] < min)
+        if ((costMatrix[indexNode][i] != 0) && (completed[i] == 0)) // if the node is not already visited and if the cost is not 0 (there is a connection)
+
+            if (costMatrix[indexNode][i] < lowestCost) // if the cost is lower than the current minimum
             {
-                min = costMatrix[i][0] + costMatrix[indexNode][i];
-                kmin = costMatrix[indexNode][i];
-                nc = i;
+                lowestCost = costMatrix[indexNode][i]; // update the minimum
+                nextNode = i;                          // update the next node
             }
     }
 
-    if (min != 999)
-        minCost += kmin;
+    if (lowestCost != 999)     // if there is a next node
+        minCost += lowestCost; // add the cost of the connection to the global cost
 
-    return nc;
+    return nextNode; // return the next node
 }
 
 /** \brief Recursive function that calculates the shortest path to visit all the nodes, as well as the minimum cost of the path
- * \param currentNode The current node
+ * \param indexNode The current node
  */
-void mincost(int node)
+void mincost(int indexNode)
 {
-    int i, nNode;
+    int nextNode; // index of the next node to visit
 
-    completed[node] = 1;
+    completed[indexNode] = 1; // mark the current node as visited
 
-    // printf("%d--->", node + 1);
-    result[resultIteration++] = node + 1;
-    nNode = least(node);
+    result[resultIteration++] = indexNode + 1; // add the next node id to the result array
 
-    if (nNode == 999)
+    nextNode = least(indexNode); // determine the next node to visit using the least function
+
+    if (nextNode == 999) // if there is no next node, we have visited all the nodes, so we return to the warehouse and stop the function
     {
-        nNode = 0;
-        // printf("%d", nNode + 1);
-        minCost += costMatrix[node][nNode];
+        nextNode = 0; // set the next node to the warehouse
 
-        return;
+        minCost += costMatrix[indexNode][nextNode]; // add the cost of the path to the warehouse
+
+        return; // stop the function
     }
 
-    mincost(nNode);
+    mincost(nextNode); // call the function again with the next node
 }
 
 /** \brief Main function : Traveling Salesman Problem (closest neighbor), will be imported in python by ctypes
